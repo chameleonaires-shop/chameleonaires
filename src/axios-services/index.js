3097fb773da602175dd0bcf16a7c3d609e181d2e
keefe089e <kelleybkeefer@googlemail.com>
2022-04-25 10:30:47 -0500

    more troubleshooting

--- src/axios-services/index.js
+++ src/axios-services/index.js
@@ -1,5 +1,5 @@
-import axios from 'axios';
-import { json } from 'express/lib/response';
+import axios from "axios";
+// import { json } from "express/lib/response";
 
 // this file holds your frontend network request adapters
 // think about each function as a service that provides data
@@ -21,48 +21,44 @@ import { json } from 'express/lib/response';
 
 export async function getAPIHealth() {
   try {
-    const { data } = await axios.get('/api/health');// can use fwtch
+    const { data } = await axios.get("/api/health"); // can use fwtch
     return data;
   } catch (err) {
     console.error(err);
     return { healthy: false };
   }
 }
-export const registerUser = async (username, password)=>{
+export const registerUser = async (username, password) => {
   try {
-    const response= await fetch(
-      '/api/users/register',
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body:JSON.stringify({user:{username,password}})
-      }
-    )
-    const data =await response.json();
-    localStorage.setItem("token",data.data.token)
+    const response = await fetch("/api/users/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ user: { username, password } }),
+    });
+    const data = await response.json();
+    localStorage.setItem("token", data.data.token);
     return data.data.token;
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-}
-export const loginUser= async (username, password)=>{
-try {
-  const response=await fetch(
-    '/api/users/login',
-    {
+};
+export const loginUser = async (username, password) => {
+  try {
+    const response = await fetch("/api/users/login", {
       method: "POST",
-      headers:{"Content-Type": "application/json"},
-      body: JSON.stringify({user:{username:username, password:password}})
-    }
-  )
-  const data=await response.json();
-  localStorage.setItem("token",data.data.token);
-  return data.data.token;
-} catch (error) {
-  console.error(error)
-}
-}
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        user: { username: username, password: password },
+      }),
+    });
+    const data = await response.json();
+    localStorage.setItem("token", data.data.token);
+    return data.data.token;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
-//    api/users/login
+//  api/users/login
